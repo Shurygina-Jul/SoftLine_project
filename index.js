2d0652f2bdd721bb15785762c4964d38d89d5e77
Shurygina-Jul <melam.2916@gmail.com>
2020-11-06 21:40:27 +0300

    функции как тип данных

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const moment = require('moment');//запись через переменные
-console.log(moment().format('DD.MM.YYYY HH:mm:ss'));
-
-var z = typeof z == 'undefined';//задание из теста
+// eslint-disable-next-line no-var
+var z = typeof z === 'undefined';// задание из теста
 console.log(z);
-
-{let age = 17;
-let restricted = age < 18 ? 'yes' : 'no';}
-//Out.log(restricted);
-
-// код функции task(x), 
-//который использует тернарный оператор для того, 
-//чтобы выдать ответ в виде строки, 
-//является ли предложенное значение значением 
-//null (выдаёт 'null'), либо undefined (выдаёт 'undefined'),
- //либо NaN (выдаёт 'not a number'). В остальных случаях должно 
- //возвращаться значение 'Other'. Например, для значения '#' 
- //должно выдаваться 'Other'.
-
-function task (x) {
-  return x === null ? 'null' : x === undefined ? 'undefined' : Number.isNaN(x) ? 'not a number' : 'Other';
+{ const age = 17;
+  const restricted = age < 18 ? 'yes' : 'no'; }
+function task(x) {
+  return x.toUpperCase();
 }
+console.log(task('sum'));
+// Out.log(restricted);
+// код функции task(x),
+// который использует тернарный оператор для того,
+// чтобы выдать ответ в виде строки,
+// является ли предложенное значение значением
+// null (выдаёт 'null'), либо undefined (выдаёт 'undefined'),
+// либо NaN (выдаёт 'not a number'). В остальных случаях должно
+// возвращаться значение 'Other'. Например, для значения '#'
+// должно выдаваться 'Other'.
+const moment = require('moment');
+
+const mom = function (f, time) { // f-функция
+  const r = time ? ' HH:mm:ss' : '';
+  const result = moment().format(`DD.MM.YYYY${r}`);
+  return f(result);
+};
+console.log(mom((x) => `<${x}>`, true));
+console.log(mom((x) => x));
+
+
